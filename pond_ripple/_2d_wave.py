69884b8fcff9723a69801955bb2ec8c115fbb361
smad0004 <smad0004@monash.edu>
2023-11-11 06:35:11 +1100

    new work, although has error

--- pond_ripple/_2d_wave.py
+++ pond_ripple/_2d_wave.py
@@ -12,6 +12,8 @@ import scipy
 
 from matplotlib.animation import FuncAnimation
 
+import math
+
 
 class wave:
 
@@ -24,11 +26,11 @@ class wave:
         self.c = None  # wave pde
 
         # spatial terms
-        self.dx = 1
+        self.dx = 0.5
         self.dy = self.dx
 
-        self.x_range = [0, 10]
-        self.y_range = [0, 12]
+        self.x_range = [0,20]
+        self.y_range = [0,30]
 
         self.x_start = start_point[0]
         self.y_start = start_point[1]
@@ -36,17 +38,17 @@ class wave:
         if not (self.x_range[1] - self.x_start)< (self.x_range[1]- self.x_range[0]) or not (self.y_range[1] - self.y_start)< (self.y_range[1]- self.y_range[0]):
             raise ValueError(f'({self.x_start,self.y_start}) not between xrange: ({self.x_range}) and/or yrange: ({self.y_range})')
         
-        self.x_points = np.arange(self.x_range[0], self.x_range[1] , self.dx, dtype = np.float32)
+        self.x_points = np.arange(self.x_range[0], self.x_range[1] + self.dx, self.dx, dtype = np.float32)
             
         self.y_points = np.arange(
-            self.y_range[0], self.y_range[1], self.dy, dtype = np.float32)
+            self.y_range[0], self.y_range[1] + self.dy, self.dy, dtype = np.float32)
 
         self.x_num_points = len(self.x_points)
         self.y_num_points = len(self.y_points)
 
         # temporal terms
         self.dt = 0.1
-        self.time_end = 2
+        self.time_end = self.dt*50
 
         self.time_points = np.arange(0,self.time_end+self.dt,self.dt)
         self.time_num_points = len(self.time_points)
@@ -72,71 +74,92 @@ class wave:
         self.initialised_field = False
 
         # sim
-        self.run_sim()
+        self.solve_using_pde_fd()
 
-    def initialise(self):
-        self.growth_rate = 1.1
-        self.curr_wave_centre = 0
-
-    def bc_check(self, position):
-        [x, y] = position
-        if not x in [self.x_range[0], self.y_range[0]] or not y in [self.x_range[1], self.y_range[1]]:
-            return True
-        else:
-            raise ValueError(
-                f'values passed to {inspect.currentframe().f_back.f_code.co_name} are not boundary values\nValues are: {position}')
-
-    def dirichlet_bc(self, position, value):
-        # check if value if boundary:
-        if self.bc_check(position):
-            return value
-
-    def neumann_bc(self, position, alpha):
-        [x, y] = position
-        self.bc_check(position)
 
     def initiliaze_field(self):
-        field = np.zeros([self.x_num_points, self.y_num_points])
+        field = np.zeros([self.y_num_points, self.x_num_points])
+        #####
+        """
+        fix initial field
+            how to create a convex wave with radius = drop radius, suppose to represent a drop of water on the pond
+        """ 
+        #####
 
         # you want a convex wave
         A = 1
         radius = 5
-        wave_length = radius*2
-        wave_func = lambda x, y: self.func(A, wave_length, x, y)
+        x0, y0 = self.x_start, self.y_start
+        wave_func = lambda x, y: 0.5*A*(np.sin( (x-x0)/(4*radius)*2*np.pi + np.pi/2 ) + np.sin((y-y0)/(4*radius)*2*np.pi + np.pi/2)) 
+        """
+        wave eqn
+        https://www.wolframalpha.com/input?i=3d+plot+sin%28x%2F%284*r%29*2*pi%2Bpi%2F2%29+%2B+sin%28y%2F%28r*4%29*2*pi%2Bpi%2F2%29+for+r+%3D+5
+        """
+        
+        i_centre = (self.x_start//self.dx)
+        j_centre = (self.y_start//self.dy)
 
+        print(f'radius = {radius}')
+        i_var = radius//self.dx*1.1
+        j_var = radius//self.dy*1.1
 
-        x_shifted = lambda i: int(i + self.x_start // self.dx) #returns pos at which x_points[pos] = x_start
-        y_shifted = lambda j: int(j + self.y_start // self.dy)
+        for i in range(int(i_centre - i_var), int(i_centre + i_var)):
+            for j in range(int(j_centre - j_var), int(j_centre + j_var)):
+                if i<0 or j<0:
+                    raise ValueError(f'i,j = {i,j}, which is wrong!!!')
+                x = self.x_points[i]
+                y = self.y_points[j]
+                #print(f'x,y = {x,y}')
 
+                if (np.power(x-self.x_start,2)+ np.power(y-self.y_start,2) > np.power(radius,2)):
+                    continue
 
+                if wave_func(x,y)>0:
+                    #print(f'i,j = {i,j} x,y = {x,y} , wave_func = {wave_func(x,y)}')
+                    field[j][i] = 1#wave_func(x,y)
 
-        for i in [i_ for i_ in range(len(self.x_points)) if self.x_points[i_] < radius and self.x_points[i_] > - radius]:
-            for j in [j_ for j_ in range(len(self.y_points)) if self.y_points[j_] < radius and self.y_points[j_] > - radius]:
-
-                field[x_shifted(i)][y_shifted(j)] = wave_func(self.x_points[i],
-                                     self.y_points[j])
         self.initialised_field = True
+
+        #self.print_Z(field)
+
+        """
+         with open('initialised_data.csv', 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerows(field)
+        """
         return field
     
     def convert_matrix_to_vector(self,matrix:np.ndarray):
-        x_,y_ = matrix.shape
-        vector = np.zeros(int(x_*y_))
-        ix = lambda x,y: self.ix(x,y)
-        for i in range(x_):
-            for j in range(y_):
-                vector[ix(i, j)] = matrix[i, j]
+        y_num,x_num = matrix.shape
+        vector = np.zeros(int(x_num*y_num))
+        ix = lambda i,j: self.ix(i,j)
+
+        for i in range(x_num):
+            for j in range(y_num):
+                vector[ix(i, j)] = matrix[j][i]
         return vector
+    
+    def convert_vector_to_matrix(self,vector:np.ndarray):
+        xi = lambda ix: self.xi(ix)
+
+        matrix = np.zeros([self.y_num_points,self.x_num_points])
+
+        for ix in range(len(vector)):
+            i,j = xi(ix)
+            matrix[j][i] = vector[ix]
+        return matrix
+
 
     def explicit_FTCS(self, initial_field_matrix: np.ndarray):
 
         nm_length = self.x_num_points*self.y_num_points
-        matrix_after_init = []
 
         vector_per_position = np.zeros(nm_length, dtype= np.float32)
 
         extra_vals_per_position = vector_per_position.copy()
 
         ix = lambda x, y: self.ix(x, y)
+        xi = lambda ix: self.xi(ix)
 
         #methods
         def fill_all_position_matrix(cur_pos_vector,i,j):
@@ -146,21 +169,6 @@ class wave:
 
             all_position_coeff_matrix[ix(i,j)][:] = cur_pos_vector
 
-        def vector_to_matrix(vector):
-            xi = lambda ix: self.xi(ix)
-
-            matrix = np.zeros([self.y_num_points,self.x_num_points])
-
-            for ix in range(len(vector)):
-                matrix[xi(ix)] = vector[ix]
-    
-            return matrix
-
-        # converting initial field condition matrix into vector and storing in all_time matrix
-        if self.initialised_field:
-            initial_field_vector = self.convert_matrix_to_vector(initial_field_matrix)
-        else:
-            raise ValueError(f'Initial Field not passed')
 
         quantities_all_time_tensor = np.zeros([self.time_num_points,self.y_num_points,self.x_num_points],dtype=np.float32) #matrix where [ [quantities @all pos @ t = 0], [quantities @all pos @ t = 0 +dt ]..... ]
         quantities_all_time_matrix = np.zeros([self.time_num_points,nm_length])
@@ -183,17 +191,23 @@ class wave:
             extra_vals --> vector containing extra values from BC or laplace module (space and/or time)
         """
  
-        for v in range(self.time_num_points):
-            time = self.time_points[v]
-            if time == 0: #ICs
-                prev_quantities_vector = initial_field_vector
-                quantities_all_time_matrix[v][:] = prev_quantities_vector #needs to updated for t>0
-                quantities_all_time_tensor[v][:][:] = vector_to_matrix(prev_quantities_vector)
+        for ti in range(self.time_num_points):
+            time = self.time_points[ti]
+
+            # initial field @t =0
+            if time == 0:
+                if not self.initialised_field: #ICs
+                    raise ValueError(f'Initial Field not passed')
+
+                prev_quantities_vector = self.convert_matrix_to_vector(initial_field_matrix)
+                quantities_all_time_matrix[ti][:] = prev_quantities_vector #needs to updated for t>0
+                quantities_all_time_tensor[ti][:][:] = self.convert_vector_to_matrix(prev_quantities_vector)
                 continue
             
             
             all_position_coeff_matrix = np.zeros([nm_length,nm_length], dtype=np.float32)
 
+            # t> 0
             for j in range(self.y_num_points):
                 for i in range(self.x_num_points):
                     x = self.x_points[i]
@@ -203,16 +217,19 @@ class wave:
                     
                     #all BCs 
                     if x in self.x_range and y in self.y_range:
+                        #corners
                         curr_position_quantities_vector[ix(i, j)] = (self.alpha*2*self.dy + prev_quantities_vector[ix(
                                         i, abs(j-2))] + self.alpha*2*self.dx + prev_quantities_vector[ix(abs(i-2), j)]) * 0.5
                         fill_all_position_matrix(curr_position_quantities_vector,i,j)
                         continue
                     elif x in self.x_range:
+                        #left right
                         curr_position_quantities_vector[ix(
                                     i, j)] = self.alpha*2*self.dx + prev_quantities_vector[ix(abs(i-2), j)]
                         fill_all_position_matrix(curr_position_quantities_vector,i,j)
                         continue
                     elif y in self.y_range:
+                        #top bott
                         curr_position_quantities_vector[ix(
                                     i, j)] = self.alpha*2*self.dy + prev_quantities_vector[ix(i, abs(j-2))]
                         fill_all_position_matrix(curr_position_quantities_vector,i,j)
@@ -227,36 +244,42 @@ class wave:
                         1 prev prev time step quantity
                     """
                     # right point
-                    curr_position_quantities_vector[ix(i+1,j)] = 1 / \
-                        np.power(self.dx, 2) * np.power(self.dt, 2) * \
-                        np.power(self.c, 2)
+                    curr_position_quantities_vector[ix(i+1,j)] = np.power(self.dt, 2) * \
+                        np.power(self.c, 2)/ \
+                        np.power(self.dx, 2) 
                     
                     # left point
-                    curr_position_quantities_vector[ix(i-1,j)] = 1 / \
-                        np.power(self.dx, 2) * np.power(self.dt, 2) * \
-                        np.power(self.c, 2)
+                    curr_position_quantities_vector[ix(i-1,j)] = np.power(self.dt, 2) * \
+                        np.power(self.c, 2) / \
+                        np.power(self.dx, 2) 
 
                     # top point
-                    curr_position_quantities_vector[ix(i,j+1)] = 1 / \
-                        np.power(self.dy, 2) * np.power(self.dt, 2) * \
-                        np.power(self.c, 2)
+                    curr_position_quantities_vector[ix(i,j+1)] = np.power(self.dt, 2) * \
+                        np.power(self.c, 2) / \
+                        np.power(self.dy, 2) 
 
                     # bott point
-                    curr_position_quantities_vector[ix(i,j-1)] = 1 / \
-                        np.power(self.dy, 2) * np.power(self.dt, 2) * \
-                        np.power(self.c, 2)
+                    curr_position_quantities_vector[ix(i,j-1)] = np.power(self.dt, 2) * \
+                        np.power(self.c, 2) / \
+                        np.power(self.dy, 2) 
 
                     # centre point
                     curr_position_quantities_vector[ix(i,j)] = 2 * \
                         (- np.power(self.c*self.dt, 2) *
-                         (1 / np.power(self.dx, 2)+1 / np.power(self.dy, 2) + 1))
+                         (1 / np.power(self.dx, 2)+ 1 / np.power(self.dy, 2) ))
 
                     # references previous time value for position
-                    extra_vals_per_position[ix(
-                        i, j)] = -1 * (prev_quantities_vector[ix(i, j)])
 
+                    if ti == self.dt:
+                        extra_vals_per_position[ix(
+                            i, j)] = -1 * (prev_quantities_vector[ix(i, j)])
+
+                    
+                    else:
+                        extra_vals_per_position[ix(
+                            i, j)] = -1 * (quantities_all_time_matrix[ti-1][ix(i, j)])
+                        
                     all_position_coeff_matrix[ix(i,j)][:] = curr_position_quantities_vector
-
             
                     """
                     TODO:
@@ -277,10 +300,12 @@ class wave:
 
             
             prev_quantities_vector = quantities_new_timestep.copy()
-            quantities_all_time_matrix[v][:] = prev_quantities_vector
+            quantities_all_time_matrix[ti][:] = prev_quantities_vector
 
-            quantities_all_time_tensor[v][:][:] = vector_to_matrix(prev_quantities_vector)
-
+            quantities_all_time_tensor[ti][:][:] = self.convert_vector_to_matrix(prev_quantities_vector)
+        self.wave_tensor = quantities_all_time_tensor
+    
+    def print_all_time_tensor(self, delay_time = 3):
         """
         spatial_matrix = np.array([self.x_num_points, self.y_num_points], dtype = np.float32)
         temporal_spatial_matrix = np.array(
@@ -298,7 +323,7 @@ class wave:
             # storing current_timestep_quantities matrix into all time tensor
             temporal_spatial_matrix[index_, :, :] = current_timestep_matrix
         """
-
+        quantities_all_time_tensor = self.wave_tensor
         # print the quantities: Q(x,y) over time
         X, Y = np.meshgrid(self.x_points, self.y_points)
         fig = plt.figure(figsize = (10,10))
@@ -312,21 +337,36 @@ class wave:
         ax.set_title(f'3D Surface Plot (t = {self.time_points[0]})')
         ax.set_zlim(-6,6)
 
-        delay_time = 3
+        ax.view_init(elev = 90, azim = 0 )
+
+        if not delay_time:
+            delay_time = 3
 
         iter = 0
 
         while True:
             plt.pause(delay_time)
+            ax.clf
 
-            #plt.clf()
             iter+=1
             iter = iter % self.time_num_points
-
+            if iter == 0:#  self.time_num_points:
+                break
+            
+            fig = plt.figure(figsize = (10,10))
+            ax = fig.add_subplot(111, projection='3d')
+            Z = quantities_all_time_tensor[0, :, :]
+            ax.plot(self.x_start,self.y_start,0,color = "k", marker= "*", markersize = 30)
+            ax.plot_surface(X, Y, Z, cmap='viridis')
+            ax.set_xlabel('X-axis')
+            ax.set_ylabel('Y-axis')
+            ax.set_zlabel('Z-axis')
+            ax.set_title(f'3D Surface Plot (t = {self.time_points[0]})')
             #ax.plot(self.x_start,self.y_start,0,color = "k", marker= "*", markersize = 30)
 
             Z = quantities_all_time_tensor[iter, :, :]
-            print(Z)
+            #print(f'time = {self.time_points[iter]}')
+            #print(Z)
             ax.plot_surface(X, Y, Z, cmap='viridis')
             
             ax.set_xlabel('X-axis')
@@ -335,51 +375,26 @@ class wave:
             ax.set_title(f'3D Surface Plot (t = {self.time_points[iter]})')
             ax.set_zlim(-6,6)
 
+
             
 
-    def run_sim(self):
-        self.solve_using_pde_fd()
-
-        # self.solve_matrix_using_vector()
-
     def solve_using_pde_fd(self):
+        #row major numbering
         Z_initial = self.initiliaze_field()
         self.explicit_FTCS(Z_initial)
+        self.print_all_time_tensor()
 
     def ix(self, i, j):
-        return (j*(self.x_num_points-1)) + i + 1 
+        return (j*(self.x_num_points)) + i 
 
     def xi(self, ix):
-        j = ix // (self.y_num_points -1 ) - 1
-        i = ix % (self.x_num_points -1 ) + 1
-        return j,i
+        #remeber its row major numbering !!
+        j = ix // (self.x_num_points)
+        i = ix % (self.x_num_points)
+        return i,j
 
-    def solve_matrix_using_vector(self):
 
-        def x_(x): return x - self.x_start
-        def y_(y): return y - self.y_start
-
-        z = np.zeros(self.m*self.n)
-        self.A = 1
-
-        for y_index in self.y_positions:
-            for x_index in self.x_positions:
-                # this is basically how you set quantities to each spatial value
-                index = int(self.ix(x_index, y_index))
-                z[index] = self.func(self.A, x_(
-                    self.x_points[x_index]), y_(self.y_points[y_index]))
-
-        def get_solution_matrix(z_vector):
-            z = np.zeros([self.m, self.n])
-            for index in range(len(z_vector)):
-                z[self.xi(index)] = z_vector[index]
-
-            return z
-
-        Z = get_solution_matrix(z)
-        self.print_solution(Z)
-
-    def print_solution(self, Z):
+    def print_Z(self, Z):
 
         X, Y = np.meshgrid(self.x_points, self.y_points)
         # Create a 3D plot
@@ -398,9 +413,6 @@ class wave:
         ax.set_xlabel('X Label')
         ax.set_ylabel('Y Label')
         ax.set_zlabel('Z Label')
-
-        # Show the plot
-        plt.show()
 
     # DEBUGGING
     def make_csv(self, input):
@@ -422,5 +434,5 @@ class wave:
 
 
 if __name__ == "__main__":
-    start_point = [5, 5]
+    start_point = [10, 10]
     wave_sim = wave(start_point)
